32ac51e405eea7436c2bd534b2dd91fb839609eb
Mika Desblancs <mika.desblancs@gmail.com>
2021-06-23 00:13:28 -0400

    - removed pesky import statements

--- YouTubeScraper.py
+++ YouTubeScraper.py
@@ -2,8 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-from selenium.common.exceptions import NoSuchElementException, TimeoutException, ElementNotInteractableException
+from selenium.common.exceptions import TimeoutException, ElementNotInteractableException
 import time
 import Video
 from requests_html import HTMLSession
